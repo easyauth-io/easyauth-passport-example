dc71fd40e167ae9d71310e08369a3e91239eadd8
jawhawk <mavenal2143@gmail.com>
2023-06-17 11:04:18 +0530

    minor fix

--- strategy.js
+++ strategy.js
@@ -7,7 +7,7 @@ passport.use(
   new EasyAuthStrategy(
     {
       discoveryURL: process.env.EASYAUTH_DISCOVERY_URL,
-      clientID: process.env.EASYAUTH_DISCOVERY_URL,
+      clientID: process.env.EASYAUTH_CLIENT_ID,
       clientSecret: process.env.EASYAUTH_CLIENT_SECRET,
       callbackURL: [`http://127.0.0.1:${PORT}/auth/easyauth/callback`], //Enter respective redirect uri set to EasyAuth ClientID.
     },
