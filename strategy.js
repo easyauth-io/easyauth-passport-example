ae3fec93f49671f4e82aeed7a1a3396639f617ac
jawhawk <mavenal2143@gmail.com>
2023-06-17 11:02:46 +0530

    .env added

--- strategy.js
+++ strategy.js
@@ -6,10 +6,10 @@ const PORT = process.env.PORT;
 passport.use(
   new EasyAuthStrategy(
     {
-      discoveryURL: process.env.DISCOVERY_URL,
-      clientID: process.env.CLIENT_ID,
-      clientSecret: process.env.CLIENT_SECRET,
-      callbackURL: [`http://127.0.0.1:${PORT}/auth/easyauth/callback`],
+      discoveryURL: process.env.EASYAUTH_DISCOVERY_URL,
+      clientID: process.env.EASYAUTH_DISCOVERY_URL,
+      clientSecret: process.env.EASYAUTH_CLIENT_SECRET,
+      callbackURL: [`http://127.0.0.1:${PORT}/auth/easyauth/callback`], //Enter respective redirect uri set to EasyAuth ClientID.
     },
     function (tokenset, userinfo, done) {
       done(null, userinfo);
